a7c4c534cea1cfb0967fa44650b03bec788a4106
Renan Machado <renan.mav@hotmail.com>
2019-11-25 16:06:53 -0300

    clean package example

--- example/src/react-app-env.d.ts
+++ example/src/react-app-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="react-scripts" />
+/// <reference types="react/experimental" />
+/// <reference types="react-dom/experimental" />
+declare module 'babel-plugin-relay/macro' {
+  import { graphql } from 'react-relay/hooks'
+  export default graphql
+}
